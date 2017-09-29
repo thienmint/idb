2b3556b9686a9f0494ce7e63367607d67f449d35
Thien Vo <jaysonvo97@gmail.com>
2017-09-29 12:12:48 -0500

    add a separate step for deployment only on dev and master, new credentials file and more debug messages

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
 
-# Set the correct project to deploy to
-if [ "$1" == "master" ]; then
-  echo "Authenticating with master project"
-  gcloud auth activate-service-account --key-file prod-client-secret.json
-  echo "Set deployment to master"
-  gcloud config set project esportguru-181021
-fi
-
-if [ "$1" == "dev" ]; then
-  echo "Authenticating with master project"
-  gcloud auth activate-service-account --key-file dev-client-secret.json
-  echo "Set deployment to dev"
-  gcloud config set project esportguru-dev
-fi
+ echo "START DEPLOYMENT: deploy.sh"
+ echo "CURRENT PATH: `pwd`"
 
  echo "Install the Python dependencies"
  pip install -r requirements.txt -t project/lib/
