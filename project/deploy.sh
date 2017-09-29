6897ee8576b3d1fb9d43b70a0ee84ea1cff36972
Thien Vo <jaysonvo97@gmail.com>
2017-09-29 00:34:03 -0500

    move the update gae-python outside of deploy code

--- project/deploy.sh
+++ project/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Set the correct project to deploy to
 if [ "$1" == "master" ]; then
   gcloud config set project esportguru-181021
 fi
@@ -7,9 +8,6 @@ fi
 if [ "$1" == "dev" ]; then
   gcloud config set project esportguru-dev
 fi
-
-# Set the correct project to deploy to
- gcloud -q components update gae-python
 
 # Install the Python dependencies
  pip install -r requirements.txt -t lib/
