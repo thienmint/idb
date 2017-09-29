af739c4a7b9667fd820091801f8704a7d91268c6
Thien Vo <jaysonvo97@gmail.com>
2017-09-29 12:44:12 -0500

    combine all step in just deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
- echo "START DEPLOYMENT: deploy.sh"
+
+ echo "START PRE-DEPLOYMENT: auth.sh"
  echo "CURRENT PATH: `pwd`"
 
+# [START auth]
+# Decrypt the credentials we added to the repo using the key we added with the Travis command line tool
+  openssl aes-256-cbc -K $encrypted_baae4e866404_key -iv $encrypted_baae4e866404_iv -in authentications.tar.gz.enc -out authentications.tar.gz -d
+
+  tar -xzf authentications.tar.gz
+  echo "MAKING SURE THAT THE KEYS ARE THERE"
+  ls -l dev-client-secret.json
+  ls -l prod-client-secret.json
+# [END auth]
 # Set the correct project to deploy to
 if [ "$1" == "master" ]; then
   echo "Authenticating with master project"
@@ -18,6 +28,8 @@ if [ "$1" == "dev" ]; then
   gcloud config set project esportguru-dev
 fi
 
+ echo "START DEPLOYMENT: deploy.sh"
+ echo "CURRENT PATH: `pwd`"
  echo "Install the Python dependencies"
  pip install -r requirements.txt -t project/lib/
 
