77152ae090908e45c8f4b09bf60dd99f5dad6e6f
Thien Vo <jaysonvo97@gmail.com>
2017-09-29 00:32:21 -0500

    add deploy script for dev and master

--- project/deploy.sh
+++ project/deploy.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-gcloud app deploy
+if [ "$1" == "master" ]; then
+  gcloud config set project esportguru-181021
+fi
+
+if [ "$1" == "dev" ]; then
+  gcloud config set project esportguru-dev
+fi
+
+# Set the correct project to deploy to
+ gcloud -q components update gae-python
+
+# Install the Python dependencies
+ pip install -r requirements.txt -t lib/
+
+# Deployment starts
+ gcloud app deploy
+# Deployment ends
