efba9e4298c69b8f2b9544f8acec7ab34b809a31
Thien Vo <jaysonvo97@gmail.com>
2017-09-29 11:13:51 -0500

    change project folder path to deploy correctly

---
+++ scripts/deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Switch to project folder
+ cd project
+
+# Set the correct project to deploy to
+if [ "$1" == "master" ]; then
+  gcloud config set project esportguru-181021
+fi
+
+if [ "$1" == "dev" ]; then
+  gcloud config set project esportguru-dev
+fi
+
+# Install the Python dependencies
+ pip install -r requirements.txt -t lib/
+
+# Deployment starts
+ gcloud app deploy
+# Deployment ends
+
+# Go back outside of project folder
+ cd ..
