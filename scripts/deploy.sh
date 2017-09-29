228cf37470fb8698875a67ad0ef29b3a3cf21480
Thien Vo <jaysonvo97@gmail.com>
2017-09-29 11:52:43 -0500

    add deployment key for dev project

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
-# Switch to project folder
- cd project
-
 # Set the correct project to deploy to
 if [ "$1" == "master" ]; then
+  echo "Authenticating with master project"
+  gcloud auth activate-service-account --key-file prod-client-secret.json
+  echo "Set deployment to master"
   gcloud config set project esportguru-181021
 fi
 
 if [ "$1" == "dev" ]; then
+  echo "Authenticating with master project"
+  gcloud auth activate-service-account --key-file dev-client-secret.json
+  echo "Set deployment to dev"
   gcloud config set project esportguru-dev
 fi
 
-# Install the Python dependencies
- pip install -r requirements.txt -t lib/
+ echo "Install the Python dependencies"
+ pip install -r requirements.txt -t project/lib/
 
-# Deployment starts
- gcloud app deploy
-# Deployment ends
-
-# Go back outside of project folder
+ cd project
+ echo "Inside project now"
+    echo "---- Deployment starts"
+    gcloud app deploy app.yaml
+    echo "---- Deployment ends"
  cd ..
+ echo "In root directory now"
