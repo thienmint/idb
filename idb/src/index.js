edcdb34bb8422491577737b803abb56adc9a2b1f
Rachel <bealerj@cs.utexas.edu>
2017-10-05 20:24:03 -0500

    made player page

--- idb/src/index.js
+++ idb/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import Home from './pages/home';
 import About from "./pages/about";
+import Players from "./pages/players";
 
-ReactDOM.render(<About />, document.getElementById('root'));
+ReactDOM.render(<Players />, document.getElementById('root'));
 registerServiceWorker();
