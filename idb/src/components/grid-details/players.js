46f5dc9cfb8dfa6897ac615e5ead8d58ddfe2d77
Rachel <bealerj@cs.utexas.edu>
2017-10-07 18:00:26 -0500

    now calling a componet with mocked data for grid, need to make it wrap with more data automatically

---
+++ idb/src/components/grid-details/players.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import './../../pages/global.css'
+
+
+class GridPlayers extends Component {
+    render() {
+        return (
+            <div className="col">
+            <div key={this.props.id}>
+                <div className="thumbnail">
+                    <a href="">
+                        <img className="img-fluid" src={this.props.value.image_url} alt={this.props.value.name}/>
+                    </a>
+                </div>
+                <div className="attributes">
+                    <div>Tag: {this.props.value.tag}</div>
+                    <div>Real name: {this.props.value.name}</div>
+                    <div>Game: <a href="">{this.props.value.current_videogame}</a></div>
+                    <div>Team: <a href="">{this.props.value.current_team}</a></div>
+                    <div>Hometown: {this.props.value.hometown}</div>
+                </div>
+            </div>
+            </div>
+        );
+    }
+}
+
+export default GridPlayers;
