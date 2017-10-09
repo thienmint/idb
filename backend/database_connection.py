88e6e4ee05c369f08a383708fe70c595bd683a5c
Persia <persia@cs.utexas.edu>
2017-10-09 17:15:51 -0500

    Created test file for db connection; works

---
+++ backend/database_connection.py
@@ -0,0 +1,15 @@
+import MySQLdb
+
+db = MySQLdb.connect(host="db.esportguru.com", 
+										 user="persia", 
+										 passwd="bigboss",
+										 db="devDB")
+
+cur = db.cursor()
+
+cur.execute("describe PLAYER")
+
+for row in cur.fetchall() : 
+	print (row[0])
+
+db.close()
