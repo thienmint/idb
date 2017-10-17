541767623953d997bc94f804fae27f3f7e52e43b
Rachel <bealerj@cs.utexas.edu>
2017-10-17 17:38:40 -0400

    changed from HashRouter to Browser Router (will need 404 page)

--- idb/src/index.js
+++ idb/src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import App from './App';
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-    <HashRouter><App/></HashRouter>, document.getElementById('root'));
+    <BrowserRouter><App/></BrowserRouter>, document.getElementById('root'));
 registerServiceWorker();
