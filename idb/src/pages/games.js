707b954f56976a901060b43e96342e036850c8c2
Rachel <bealerj@cs.utexas.edu>
2017-10-19 13:08:19 -0400

    updated game page to use grid component

--- idb/src/pages/games.js
+++ idb/src/pages/games.js
@@ -5,59 +5,79 @@ import './global.css'
 import League from '../static/images/League_of_Legends_logo.png';
 import CSGO from '../static/images/csgo.png';
 import SSBM from '../static/images/ssbm.jpeg';
+import GridGames from "../components/grid-details/games";
 
 class Games extends Component {
+
+    game_data = [
+        {
+            id: 1,
+            image_url: League,
+            name: 'League of Legends',
+            year: '2009',
+            company: 'Riot Games',
+            genre: 'MOBA',
+            champion: 'SKT T1',
+            teams: ['Cloud9', 'SoloMid', 'Liquid', 'Fnatic'],
+            tournaments: ['LCS Europe', 'LCS North America', 'IEM World Championships']
+        },
+        {
+            id: 2,
+            image_url: CSGO,
+            name: 'Counter-Strike: Global Offensive',
+            year: '2012',
+            company: 'Valve',
+            genre: 'First person shooter',
+            champion: 'SK Gaming',
+            teams: ['Cloud9', 'Fnatic', 'Liquid'],
+            tournaments: ['Dreamhack', 'Intel Extreme Masters']
+        },
+        {
+            id: 3,
+            image_url: SSBM,
+            name: 'Super Smash Bros. Melee',
+            year: '2001',
+            company: 'HAL Laboratory',
+            genre: 'Fighting Game',
+            champion: 'Armada',
+            teams: ['Alliance', 'Liquid', 'SoloMid', 'Cloud9'],
+            tournaments: ['Apex', 'DreamHack', 'Genesis']
+        },
+        {
+            id: 4,
+            image_url: CSGO,
+            name: 'League of Legends',
+            year: '2012',
+            company: 'HAL Laboratory',
+            genre: 'MOBA',
+            champion: 'Armada',
+            teams: ['Cloud9', 'SoloMid', 'Liquid', 'Fnatic'],
+            tournaments: ['Apex', 'DreamHack', 'Genesis']
+        }
+    ];
+
     render() {
+        let numRows = Math.ceil(this.game_data.length / 3);
+        let rows = [];
+        let cols = [];
+        for (let i=0; i< numRows; i++) {
+            cols = this.game_data.splice(0,3);
+            rows.push(
+                <div className="row align-items-start">
+                    {cols.map((team, index) => (
+                        <GridGames value={team} key={index}/>
+                    ))}
+                </div>
+            )
+        }
+
         return (
             <div>
                 <Navbar/>
                 <h1 className="page-title">Games</h1>
                 <hr/>
                 <div className="container">
-                    <div className="row align-items-start">
-                        <div className="col">
-                            <div className="thumbnail">
-                                <a href="">
-                                    <img className="img-fluid" src={League} alt=""/>
-                                </a>
-                            </div>
-                            <div className="attributes">
-                                <div>Name: League of Legends</div>
-                                <div>Year published: 2009</div>
-                                <div>Company: Riot Games</div>
-                                <div>Genre: MOBA</div>
-                                <div>Current Champion: SKT T1</div>
-                            </div>
-                        </div>
-                        <div className="col">
-                            <div className="thumbnail">
-                                <a href="">
-                                    <img className="img-fluid" src={SSBM} alt=""/>
-                                </a>
-                            </div>
-                            <div className="attributes">
-                                <div>Name: SSBM</div>
-                                <div>Year published: 2001</div>
-                                <div>Company: HAL Laboratory</div>
-                                <div>Genre: Fighting Game</div>
-                                <div>Current Champion: <a href="">Armada</a></div>
-                            </div>
-                        </div>
-                        <div className="col">
-                            <div className="thumbnail">
-                                <a href="">
-                                    <img className="img-fluid" src={CSGO} alt=""/>
-                                </a>
-                            </div>
-                            <div className="attributes">
-                                <div>Name: CSGO</div>
-                                <div>Year published: 2012</div>
-                                <div>Company: Valve</div>
-                                <div>Genre: First person shooter</div>
-                                <div>Current Champion: SK Gaming</div>
-                            </div>
-                        </div>
-                    </div>
+                    {rows}
                 </div>
             </div>
         );
