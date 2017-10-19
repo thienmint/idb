327565824eddb9fba974e90703a7add59cb51ecb
Rachel <bealerj@cs.utexas.edu>
2017-10-19 12:54:56 -0400

    converted tournament page to use grid component

--- idb/src/pages/tournaments.js
+++ idb/src/pages/tournaments.js
@@ -5,59 +5,74 @@ import './global.css'
 import Dreamhack from '../static/images/dreamhack.png';
 import NALCS from '../static/images/league-championships.jpeg';
 import TI7 from '../static/images/int7.jpg';
+import GridTournaments from "../components/grid-details/tournaments";
 
 class Tournaments extends Component {
+
+    tournament_data = [
+        {
+            id: 1,
+            image_url: Dreamhack,
+            name: 'DreamHack Summer 2017',
+            game: 'CSGO',
+            city: 'Jönköping',
+            teams: ['SKGaming', 'Fnatic', 'Immortals', 'Counter Logic', 'mousesports',
+                'Gambit Esports', 'Team Singularity', 'Cloud9'],
+            winner: ['SKGaming']
+        },
+        {
+            id: 2,
+            image_url: NALCS,
+            name: 'NA LCS Summer Split 2017',
+            game: 'League of Legends',
+            city: 'San Fransisco',
+            teams: ['Cloud 9', 'Counter Logic Gaming', 'Echo Fox', 'FlyQuest eSports',
+                'Immortals', 'Phoenix1', 'Team Dignitas', 'Team EnVyUs', 'Team Liquid'],
+            winner: ['Team SoloMid']
+        },
+        {
+            id: 3,
+            image_url: TI7,
+            name: 'DreamHack Summer 2017',
+            game: 'CSGO',
+            city: 'Jönköping',
+            teams: ['SKGaming', 'Fnatic', 'Immortals', 'Counter Logic', 'mousesports',
+                'Gambit Esports', 'Team Singularity', 'Cloud9'],
+            winner: ['SKGaming']
+        },
+        {
+            id: 4,
+            image_url: NALCS,
+            name: 'DreamHack Summer 2017',
+            game: 'CSGO',
+            city: 'Jönköping',
+            teams: ['SKGaming', 'Fnatic', 'Immortals', 'Counter Logic', 'mousesports',
+                'Gambit Esports', 'Team Singularity', 'Cloud9'],
+            winner: ['SKGaming']
+        }
+    ];
+
     render() {
+        let numRows = Math.ceil(this.tournament_data.length / 3);
+        let rows = [];
+        let cols = [];
+        for (let i=0; i< numRows; i++) {
+            cols = this.tournament_data.splice(0,3);
+            rows.push(
+                <div className="row align-items-start">
+                    {cols.map((team, index) => (
+                        <GridTournaments value={team} key={index}/>
+                    ))}
+                </div>
+            )
+        }
         return (
             <div>
                 <Navbar/>
                 <h1 className="page-title">Tournaments</h1>
                 <hr/>
                 <div className="container">
-                    <div className="row align-items-start">
-                        <div className="col">
-                            <div className="thumbnail">
-                                <a href="">
-                                    <img className="img-fluid" src={Dreamhack} alt=""/>
-                                </a>
-                            </div>
-                            <div className="attributes">
-                                <div>Name: DreamHack Summer 2017</div>
-                                <div><a href="./csgo"> Game: CSGO</a></div>
-                                <div>City: Jönköping</div>
-                                <div>Number of Attendees: 8 teams</div>
-                                <div>Winner: SK Gaming</div>
-                            </div>
-                        </div>
-                        <div className="col">
-                            <div className="thumbnail">
-                                <a href="">
-                                    <img className="img-fluid" src={NALCS} alt=""/>
-                                </a>
-                            </div>
-                            <div className="attributes">
-                                <div>Name: NA LCS Summer Split 2017</div>
-                                <div><a href=""> Game: League of Legends</a></div>
-                                <div>City: San Francisco</div>
-                                <div>Number of Attendees: 8 teams</div>
-                                <div>Winner: <a href="">Team SoloMid</a></div>
-                            </div>
-                        </div>
-                        <div className="col">
-                            <div className="thumbnail">
-                                <a href="">
-                                    <img className="img-fluid" src={TI7} alt=""/>
-                                </a>
-                            </div>
-                            <div className="attributes">
-                                <div>Name: The International 2017</div>
-                                <div>Game: Dota 2</div>
-                                <div>City: Seattle</div>
-                                <div>Number of Attendees: 18</div>
-                                <div>Winner: <a href="">Team Liquid</a></div>
-                            </div>
-                        </div>
-                    </div>
+                    {rows}
                 </div>
             </div>
         );
