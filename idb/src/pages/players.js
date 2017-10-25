76f86a3f4d261ea999f61c0868a82204f127a242
Rachel <bealerj@cs.utexas.edu>
2017-10-24 21:16:42 -0500

    pls help

--- idb/src/pages/players.js
+++ idb/src/pages/players.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Navbar from './../components/nav/navbar';
 import './global.css'
+import axios from 'axios';
 
 import Adam from '../static/images/adam.jpg';
 import Nikola from '../static/images/niki.jpeg';
@@ -8,7 +9,34 @@ import Yilliang from '../static/images/yiliang_peng.jpg';
 import GridPlayers from "../components/grid-details/players";
 
 
-class Players extends Component {
+export default class Players extends Component {
+
+    componentWillMount() {
+        this.getData();
+    }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            players: [],
+        };
+    }
+
+    getData() {
+        let proxyurl = 'https://cors-anywhere.herokuapp.com/';
+        let apiurl = 'http://api.esportguru.com/';
+        axios.get(proxyurl + apiurl + 'players').then((response) => {
+
+            let stateCopy = Object.assign({}, this.state);
+            stateCopy.players = stateCopy.players.slice();
+            stateCopy.players = Object.assign({}, response.data);
+            this.setState(stateCopy);
+            this.setState(this.state);
+        }).catch(function (error) {
+            console.log(error);
+        });
+    }
+
     player_data = [
         {
             id: 1,
@@ -61,30 +89,61 @@ class Players extends Component {
     ];
 
     render() {
-        let numRows = Math.ceil(this.player_data.length / 3);
-        let rows = [];
-        let cols = [];
-        for (let i=0; i< numRows; i++) {
-            cols = this.player_data.splice(0,3);
-            rows.push(
-                <div className="row align-items-start">
-                    {cols.map((player, index) => (
-                        <GridPlayers value={player} key={index}/>
-                    ))}
-                </div>
-            )
+        let numRows = Math.ceil(Object.keys(this.state.players).length / 3);
+        let grid = [];
+        let row = [];
+        for(let i = 0; i < numRows; i++){
+            row = Object.values(this.state.players).splice(0,3);
+            grid.push(row);
         }
+        console.log('in main render - logging grid before passing to playerRow');
+        console.log(grid);
       return (
             <div>
                 <Navbar/>
                 <h1 className="page-title">Players</h1>
                 <hr/>
                 <div className="container">
-                    {rows}
+                    {grid.map((item, index) => (
+                        <PlayerRow values={item} key={index}/>
+                    ))}
                 </div>
             </div>
         );
     }
 }
 
-export default Players;
+class PlayerRow extends Component {
+    render() {
+        // console.log(this.props.values);
+        // let numRows = Math.ceil(Object.keys(this.props.values).length / 3);
+        // let rows = [];
+        // let val = [];
+        // for (let i=0; i< numRows; i++) {
+        //     val = Object.values(this.props.values).splice(0,3);
+        //     console.log('in player row... logging value');
+        //     console.log(val);
+        //     rows.push(val);
+        // }
+
+        console.log('in player row');
+        let row = this.props.values;
+        let players = [];
+        players.push(
+            row.map((player, index) => (
+                React.createElement(GridPlayers, {value: player})
+            ))
+        );
+        console.log(row);
+        console.log('loggin players before shit blows up');
+        console.log(players);
+    return (
+        <div className="row align-items-start">
+            <GridPlayers value={row}/>
+        </div>
+        );
+    }
+
+// <div className="row align-items-start" key={i}>
+// </div>
+}
