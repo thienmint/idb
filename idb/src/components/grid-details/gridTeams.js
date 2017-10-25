8c206465554237b73e8347ea5769b15095586df1
Rachel <bealerj@cs.utexas.edu>
2017-10-24 23:07:23 -0500

    and now we have teams talking to the backend!

--- idb/src/components/grid-details/gridTeams.js
+++ idb/src/components/grid-details/gridTeams.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './../../pages/global.css'
+import NotFound from './../../static/images/image-not-found.png'
 
 class GridTeams extends Component {
     render() {
@@ -8,16 +9,23 @@ class GridTeams extends Component {
                 <div key={this.props.id}>
                     <div className="thumbnail">
                         <a href="">
-                            <img className="img-fluid" src={this.props.value.logo_url} alt={this.props.value.name}/>
-                        </a>
+                            {this.props.value.image_url &&
+                            <img className="img-fluid" src={this.props.value.image_url} alt={this.props.value.name}/>
+                            }
+                            {!this.props.value.image_url &&
+                            <img className="img-fluid" src={NotFound} alt={this.props.value.name}/>
+                            }
+                            </a>
                     </div>
                     <div className="attributes">
                         <div>Name: {this.props.value.name}</div>
-                        <div>Games: {this.props.value.games}</div>
-                        <div>Roster: {this.props.roster}</div>
-                        <div>Date founded: {this.props.value.date}</div>
-                        <div>Championships: {this.props.value.championships}</div>
-                        <div>Location: {this.props.value.location}</div>
+                        {this.props.value.current_game ?
+                            <div>Current Game: {this.props.value.current_game.name}</div> :
+                            <div>Current Game: None found</div>}
+                        {this.props.value.current_players[0] !== undefined ?
+                            <div>Current Players: {this.props.value.current_players[0].tag}</div> :
+                            <div>Current Players: None found</div>}
+                        <div>Acronym: {this.props.value.acronym}</div>
                     </div>
                 </div>
             </div>
@@ -26,3 +34,10 @@ class GridTeams extends Component {
 }
 
 export default GridTeams;
+
+/* acronym:"GV"
+current_game:{id: 1, name: "League of Legends"}
+current_players:[]
+id:9
+image_url:"https://pandacdn.blob.core.windows.net/cdn/uploads/gravity-fg1nz3w2.png"
+name: "Gravity"*/
--- idb/src/pages/teams.js
+++ idb/src/pages/teams.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
 import Navbar from './../components/nav/navbar';
 import './global.css'
+import axios from 'axios';
 
 import SoloMid from '../static/images/soloMid.jpg';
 import Fnatic from '../static/images/fnatic-logo-font.png';
 import Liquid from '../static/images/team-liquid-logo-B045C2BBCC-seeklogo.com.png';
 import GridTeams from "../components/grid-details/gridTeams";
 
-class Teams extends Component {
+export default class Teams extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            teams: [],
+        };
+
+        let proxyurl = 'https://cors-anywhere.herokuapp.com/';
+        let apiurl = 'http://api.esportguru.com/';
+        axios.get(proxyurl + apiurl + 'teams').then((response) => {
+            let stateCopy = Object.assign({}, this.state);
+            stateCopy.teams = stateCopy.teams.slice();
+            stateCopy.teams = Object.assign({}, response.data);
+            this.setState(stateCopy);
+            console.log(this.state.teams);
+        }).catch(function (error) {
+            console.log(error);
+        });
+    }
+
     team_data = [
         {
             id: 1,
@@ -52,18 +73,13 @@ class Teams extends Component {
     ];
 
     render() {
-        let numRows = Math.ceil(this.team_data.length / 3);
-        let rows = [];
-        let cols = [];
-        for (let i=0; i< numRows; i++) {
-            cols = this.team_data.splice(0,3);
-            rows.push(
-                <div className="row align-items-start">
-                    {cols.map((team, index) => (
-                        <GridTeams value={team} key={index}/>
-                    ))}
-                </div>
-            )
+        let numRows = Math.ceil(Object.keys(this.state.teams).length / 3);
+        let teams = Object.values(this.state.teams);
+        let grid = [];
+        let row = [];
+        for(let i = 0; i < numRows; i++){
+            row = teams.splice(0,3);
+            grid.push(row);
         }
         return (
             <div>
@@ -71,11 +87,28 @@ class Teams extends Component {
                 <h1 className="page-title">Teams</h1>
                 <hr/>
                 <div className="container">
-                    {rows}
+                    {grid.map((item, index) => (
+                        <TeamRow values={item} key={index}/>
+                    ))}
                 </div>
             </div>
         );
     }
 }
 
-export default Teams;
+class TeamRow extends Component {
+    render() {
+        let row = this.props.values;
+        let teams = [];
+        teams.push(
+            row.map((team, index) => (
+                React.createElement(GridTeams, {value: team})
+            ))
+        );
+        return (
+            <div className="row align-items-start">
+                {teams}
+            </div>
+        );
+    }
+}
