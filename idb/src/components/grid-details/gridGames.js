d9050c5b928eae2d00efff1d7f98a50aeb05c3d1
Rachel <bealerj@cs.utexas.edu>
2017-10-24 22:49:43 -0500

    we now have games talking to the backend

--- idb/src/components/grid-details/gridGames.js
+++ idb/src/components/grid-details/gridGames.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './../../pages/global.css'
+import NotFound from './../../static/images/image-not-found.png'
+
 
 class GridGames extends Component {
     render() {
@@ -8,16 +10,19 @@ class GridGames extends Component {
                 <div key={this.props.id}>
                     <div className="thumbnail">
                         <a href="">
-                            <img className="img-fluid" src={this.props.value.image_url} alt={this.props.value.name}/>
+                            {this.props.value.screenshots &&
+                            <img className="img-fluid" src={this.props.value.screenshots[0]} alt={this.props.value.name}/>
+                            }
+                            {!this.props.value.screenshots &&
+                            <img className="img-fluid" src={NotFound} alt={this.props.value.name}/>
+                            }
                         </a>
                     </div>
                     <div className="attributes">
-                        <div>Year Published: {this.props.value.year}</div>
-                        <div>Company: {this.props.value.company}</div>
-                        <div>Genre: {this.props.genre}</div>
-                        <div>Current Champion: {this.props.value.champion}</div>
-                        <div>Teams: {this.props.value.teams}</div>
-                        <div>Tournaments: {this.props.value.tournaments}</div>
+                        <div>Name: {this.props.value.name}</div>
+                        <div>Summary: {this.props.value.summary}</div>
+                        <div>Release Date: {this.props.release_date}</div>
+                        <div>Websites: {this.props.value.website[0]}</div>
                     </div>
                 </div>
             </div>
