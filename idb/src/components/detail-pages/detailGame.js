091d2e6c00acc2afcb375108a9c208130e877545
Rachel <bealerj@cs.utexas.edu>
2017-10-26 01:00:47 -0500

    update detail page for games to all players and teams

--- idb/src/components/detail-pages/detailGame.js
+++ idb/src/components/detail-pages/detailGame.js
@@ -4,6 +4,7 @@ import './../../pages/global.css'
 import './detail.css'
 import axios from 'axios';
 import NotFound from './../../static/images/image-not-found.png'
+import { Link } from 'react-router-dom';
 
 export default class DetailGame extends Component {
 
@@ -66,6 +67,26 @@ export default class DetailGame extends Component {
                                             <div><a href={site}>{site}</a></div>
                                         ))}
                                     </div>
+                            </div>
+                            }
+                            {this.state.game.sample_players &&
+                            <div className="row align-items-start">
+                                <div className="col-4 offset-2 labels lists">Players</div>
+                                <div className="col-4 lists detail-columns">
+                                    {this.state.game.sample_players.map((player, index) => (
+                                        <div><Link to={`/players/${player.id}`}>{player.tag}</Link></div>
+                                    ))}
+                                </div>
+                            </div>
+                            }
+                            {this.state.game.sample_teams &&
+                            <div className="row align-items-start">
+                                <div className="col-4 offset-2 labels lists">Teams</div>
+                                <div className="col-4 lists detail-columns">
+                                    {this.state.game.sample_teams.map((team, index) => (
+                                        <div><Link to={`/teams/${team.id}`}>{team.name}</Link></div>
+                                    ))}
+                                </div>
                             </div>
                             }
                         </div>
