682cfb19f1c321e47ad3c7f4c85f540d96d23beb
Rachel <bealerj@cs.utexas.edu>
2017-10-26 00:05:32 -0500

    got rid of unnecessary static imgs

--- idb/src/pages/games.js
+++ idb/src/pages/games.js
@@ -3,9 +3,6 @@ import Navbar from './../components/nav/navbar';
 import './global.css'
 import axios from 'axios';
 
-import League from '../static/images/League_of_Legends_logo.png';
-import CSGO from '../static/images/csgo.png';
-import SSBM from '../static/images/ssbm.jpeg';
 import GridGames from "../components/grid-details/gridGames";
 import { DotLoader } from 'react-spinners';
 
@@ -30,53 +27,6 @@ export default class Games extends Component {
             console.log(error);
         });
     }
-
-    game_data = [
-        {
-            id: 1,
-            image_url: League,
-            name: 'League of Legends',
-            year: '2009',
-            company: 'Riot Games',
-            genre: 'MOBA',
-            champion: 'SKT T1',
-            teams: ['Cloud9', 'SoloMid', 'Liquid', 'Fnatic'],
-            tournaments: ['LCS Europe', 'LCS North America', 'IEM World Championships']
-        },
-        {
-            id: 2,
-            image_url: CSGO,
-            name: 'Counter-Strike: Global Offensive',
-            year: '2012',
-            company: 'Valve',
-            genre: 'First person shooter',
-            champion: 'SK Gaming',
-            teams: ['Cloud9', 'Fnatic', 'Liquid'],
-            tournaments: ['Dreamhack', 'Intel Extreme Masters']
-        },
-        {
-            id: 3,
-            image_url: SSBM,
-            name: 'Super Smash Bros. Melee',
-            year: '2001',
-            company: 'HAL Laboratory',
-            genre: 'Fighting Game',
-            champion: 'Armada',
-            teams: ['Alliance', 'Liquid', 'SoloMid', 'Cloud9'],
-            tournaments: ['Apex', 'DreamHack', 'Genesis']
-        },
-        {
-            id: 4,
-            image_url: CSGO,
-            name: 'League of Legends',
-            year: '2012',
-            company: 'HAL Laboratory',
-            genre: 'MOBA',
-            champion: 'Armada',
-            teams: ['Cloud9', 'SoloMid', 'Liquid', 'Fnatic'],
-            tournaments: ['Apex', 'DreamHack', 'Genesis']
-        }
-    ];
 
     render() {
         let numRows = Math.ceil(Object.keys(this.state.games).length / 3);
