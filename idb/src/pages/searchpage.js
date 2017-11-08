d3403b9e183809df77f639b22c3b577a622c2b27
Eric Liu <liueric@utexas.edu>
2017-11-08 15:45:57 -0600

    search grid in progress

--- idb/src/pages/searchpage.js
+++ idb/src/pages/searchpage.js
@@ -3,7 +3,7 @@ import Navbar from './../components/nav/navbar';
 import './global.css'
 import axios from 'axios';
 
-import GridGames from "../components/grid-details/gridGames";
+import GridGames from "../components/grid-details/gridSearch";
 import { DotLoader } from 'react-spinners';
 
 export default class SearchPage extends Component {
@@ -16,7 +16,7 @@ export default class SearchPage extends Component {
         };
 
         let apiurl = 'http://api.esportguru.com/';
-        axios.get(apiurl + 'search').then((response) => {
+        axios.get(apiurl + 'games').then((response) => {
             let stateCopy = Object.assign({}, this.state);
             stateCopy.games = stateCopy.games.slice();
             stateCopy.games = Object.assign({}, response.data);
