594a3d5400639e1dc9a67dbe055005f78b0b0f67
Thien Vo <jaysonvo97@gmail.com>
2017-11-08 00:58:31 -0600

    finish sorting for tournaments

--- idb/src/pages/tournaments.js
+++ idb/src/pages/tournaments.js
@@ -13,34 +13,130 @@ export default class Tournaments extends Component {
         this.state = {
             tournaments: [],
             loading: true,
+            grid: [],
+            sortOpt: 'Name',
+            sortOrder: 'default'
         };
 
         let apiurl = 'http://api.esportguru.com/';
         axios.get(apiurl + 'tournaments').then((response) => {
-            let stateCopy = Object.assign({}, this.state);
+            let stateCopy = Object.assign([], this.state);
             stateCopy.tournaments = stateCopy.tournaments.slice();
-            stateCopy.tournaments = Object.assign({}, response.data);
+            stateCopy.tournaments = Object.assign([], response.data);
+            stateCopy.grid = Tournaments.makeGrid(stateCopy.tournaments);
             stateCopy.loading = false;
             this.setState(stateCopy);
         }).catch(function (error) {
             console.log(error);
         });
+
+        this.sortOptChange = this.sortOptChange.bind(this);
+        this.sortHandle = this.sortHandle.bind(this);
+        this.sortGrid = this.sortGrid.bind(this);
     }
 
-    render() {
-        let numRows = Math.ceil(Object.keys(this.state.tournaments).length / 3);
-        let tournaments = Object.values(this.state.tournaments);
+    static makeGrid(tourneyState) {
+        let numItemPerRow = 3;
+        let numRows = Math.ceil(Object.keys(tourneyState).length / numItemPerRow);
+        let tournaments = Object.values(tourneyState);
         let grid = [];
         let row = [];
         for(let i = 0; i < numRows; i++){
-            row = tournaments.splice(0,3);
+            row = tournaments.splice(0, numItemPerRow);
             grid.push(row);
         }
+        return grid
+    }
+
+    sortHandle(event) {
+        let stateCopy = Object.assign([], this.state);
+        stateCopy.sortOrder = event.target.value;
+        this.sortGrid(stateCopy)
+    }
+
+    sortOptChange(event) {
+        let stateCopy = Object.assign([], this.state);
+        stateCopy.sortOpt = event.target.value;
+        this.sortGrid(stateCopy)
+    }
+    static compareString(x, y) {
+        if(x === null && y !== null)
+            return -1;
+        else if(x !== null && y === null)
+            return 1;
+        else if(x === null && y === null)
+            return 0;
+        else
+            return x.toLowerCase().localeCompare(y.toLowerCase())
+    }
+
+    sortGrid(stateCopy) {
+        // console.log("SortGrid")
+        switch (stateCopy.sortOrder) {
+            case "asc":
+                switch (stateCopy.sortOpt) {
+                    case "Name":
+                        stateCopy.tournaments = stateCopy.tournaments.sort((x, y) => (Tournaments.compareString(x.name, y.name)));
+                        break;
+                    case "Slug":
+                        stateCopy.tournaments = stateCopy.tournaments.sort((x, y) => (Tournaments.compareString(x.slug, y.slug)));
+                        break;
+                    case "StartDate":
+                        stateCopy.tournaments = stateCopy.tournaments.sort((x, y) => (Tournaments.compareString(x.begin_at, y.begin_at)));
+                        break;
+                    case "EndDate":
+                        stateCopy.tournaments = stateCopy.tournaments.sort((x, y) => (Tournaments.compareString(x.end_at, y.end_at)));
+                        break;
+                } break;
+            case "desc":
+                switch (stateCopy.sortOpt) {
+                    case "Name":
+                        stateCopy.tournaments = stateCopy.tournaments.sort((x, y) => (Tournaments.compareString(y.name, x.name)));
+                        break;
+                    case "Slug":
+                        stateCopy.tournaments = stateCopy.tournaments.sort((x, y) => (Tournaments.compareString(y.slug, x.slug)));
+                        break;
+                    case "StartDate":
+                        stateCopy.tournaments = stateCopy.tournaments.sort((x, y) => (Tournaments.compareString(y.begin_at, x.begin_at)));
+                        break;
+                    case "EndDate":
+                        stateCopy.tournaments = stateCopy.tournaments.sort((x, y) => (Tournaments.compareString(y.end_at, x.end_at)));
+                        break;
+                } break;
+            default: stateCopy.tournaments = this.state.players;
+        }
+
+        stateCopy.loading = false;
+        stateCopy.grid = Tournaments.makeGrid(stateCopy.tournaments);
+
+        this.setState(stateCopy)
+    }
+
+    render() {
+
         return (
             <div>
                 <Navbar/>
                 <h1 className="page-title">Tournaments</h1>
                 <hr/>
+
+                <p>Sort by: &nbsp;
+                    <select value={this.state.sortOpt} onChange={this.sortOptChange}>
+                        <option value="Name">Name</option>
+                        <option value="Slug">Slug</option>
+                        <option value="StartDate">Start Date</option>
+                        <option value="EndDate">End Date</option>
+                    </select>
+                    &nbsp;
+                    <select value={this.state.sortOrder} onChange={this.sortHandle}>
+                        <option value="default" className="default-option">Select</option>
+                        <option value="asc">Ascending</option>
+                        <option value="desc">Descending</option>
+                    </select>
+                </p>
+
+                <br/>
+
                 {this.state.loading ?
                     <div className="loading">
                         <DotLoader
@@ -51,7 +147,7 @@ export default class Tournaments extends Component {
                     </div>
                     :
                     <div className="container">
-                        {grid.map((item, index) => (
+                        {this.state.grid.map((item, index) => (
                             <TournamentRow values={item} key={index}/>
                         ))}
                     </div>
