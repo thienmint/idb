24c517d1cea5048909493b582f984d89649eab2e
Rachel <bealerj@cs.utexas.edu>
2017-11-09 17:04:38 -0600

    get rid of most compiler warnings

--- idb/src/components/grid-details/gridSearchTeams.js
+++ idb/src/components/grid-details/gridSearchTeams.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './../../pages/global.css'
 import { Link } from 'react-router-dom';
-import NotFound from './../../static/images/image-not-found.png'
 
 class GridSearchTeams extends Component {
 
